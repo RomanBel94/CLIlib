fcad3ccb96a230c1769bad3546b4523bf8044d59
Roman Belyaev <rsbel94@ya.ru>
2025-01-12 15:04:26 +0300

    added test for empty argv

--- src/test.cpp
+++ src/test.cpp
@@ -7,13 +7,26 @@
 auto cli = CLI::CLI::get_instance();
 std::list<CLI::CLI::token> expected_token_list;
 
-TEST(CLI_ShortKeys, SingleKey)
+TEST(CLI_EmptyArgv, NoArgumentsGiven)
 {
     // Arrange
-    const char* args_pack1[] = {"", "-t"};
+    const char* args_pack1[] = {""};
+    // Act
+    cli->parse_args(1, const_cast<char**>(args_pack1));
+    // Assert
+    EXPECT_EQ(cli->tokens(), expected_token_list);
+    // clearing test case
+    cli->clear();
+    expected_token_list.clear();
+}
+
+TEST(CLI_ShortKeys, SingleShortKey)
+{
+    // Arrange
+    const char* args_pack2[] = {"", "-t"};
     expected_token_list.emplace_back("t", "");
     // Act
-    cli->parse_args(2, const_cast<char**>(args_pack1));
+    cli->parse_args(2, const_cast<char**>(args_pack2));
     // Assert
     EXPECT_EQ(cli->tokens(), expected_token_list);
     // clearing test case
