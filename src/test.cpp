0870ddd92a69de1621a4ac74cf7920cd5cfe021b
Roman Belyaev <rsbel94@ya.ru>
2025-01-12 14:58:23 +0300

    added first google test

--- src/test.cpp
+++ src/test.cpp
@@ -1,10 +1,31 @@
-#include "../googletest/googletest/include/gtest/gtest.h"
 #include "CLIlib.h"
 #include <list>
 
+// has to be included after all other includes
+#include "../googletest/googletest/include/gtest/gtest.h"
+
+auto cli = CLI::CLI::get_instance();
+std::list<CLI::CLI::token> expected_token_list;
+
+TEST(CLI_ShortKeys, SingleKey)
+{
+    // Arrange
+    const char* args_pack1[] = {"", "-t"};
+    expected_token_list.emplace_back("t", "");
+    // Act
+    cli->parse_args(2, const_cast<char**>(args_pack1));
+    // Assert
+    EXPECT_EQ(cli->tokens(), expected_token_list);
+    // clearing test case
+    cli->clear();
+    expected_token_list.clear();
+}
+
 int main(int argc, char** argv)
 {
+    // inits googletest framework
     testing::InitGoogleTest(&argc, argv);
 
+    // runs all tests
     return RUN_ALL_TESTS();
 }
